4c64e41af1eb40f2ffe9aa472aa937ff567fe3f6
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-20 02:05:29 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,64 @@ class Song
     self.class.all << self
   end
 
+   def self.create
+    song = self.new
+    @@all << song
+    return song
+  end
+    
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    return song
+    
+  end
+  
+  def self.find_by_name(name)
+    song = self.all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song == nil
+      song = self.create_by_name(name)
+    end
+    return song
+  end
+        
+  def self.alphabetical
+    sorted = self.all.sort_by {|song| song.name}
+    return sorted
+  end
+    
+  def self.new_from_filename(filename)
+    nameWithoutExtension = filename[0..-5]
+    splitName = nameWithoutExtension.split(" - ")
+    
+    song = self.new
+    song.artist_name = splitName[0]
+    song.name = splitName[1]
+    return song
+  end
+    
+  def self.create_from_filename(filename)
+     nameWithoutExtension = filename[0..-5]
+    splitName = nameWithoutExtension.split(" - ")
+    
+    song = self.create
+    song.artist_name = splitName[0]
+    song.name = splitName[1]
+    return song
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
 end
