a995388d460731349d1c377476005d41aaa3e66b
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-20 01:02:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,14 @@ class Song
   end
     
   def self.new_by_name(name)
-   
+    song = self.new
+    song.name = name
+    return song
   end
+  
   def self.create_by_name(name)
-    
+    song = self.create
+    song.name = name
+    return song
+  end
 end
